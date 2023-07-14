c06a524dd2d7c2c4aecf5e0cbc217659686752de
Subhajit Das <babai.dassubhajit@gmail.com>
2023-07-14 20:28:49 +0530

    db updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 // const URI = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@cluster0.covjhoe.mongodb.net/' + process.env.DB_NAME + '?retryWrites=true&w=majority';
-const URI = "mongodb+srv://hawt_sauce:VmDA5oqizhTwIQdt@cluster0.covjhoe.mongodb.net/?retryWrites=true&w=majority";
+const URI = "mongodb+srv://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@cluster0.covjhoe.mongodb.net/?retryWrites=true&w=majority";
+
 
 const connectDB = async () => {
   try {
